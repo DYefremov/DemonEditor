2dcc9a85b511770115a1f0c1374683690e0ad7cd
DYefremov <dmitriy.v.yefremov@gmail.com>
2019-10-23 12:38:23 +0300

    updated app bar menu

--- start.py
+++ start.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+
+try:
+    from Cocoa import NSBundle
+except ImportError as e:
+    print(e)
+else:
+    ns_bundle = NSBundle.mainBundle()
+    if ns_bundle:
+        ns_bundle = ns_bundle.localizedInfoDictionary() or ns_bundle.infoDictionary()
+        if ns_bundle:
+            ns_bundle["CFBundleName"] = "DemonEditor"
+
 from app.ui.main_app_window import start_app
 
 start_app()
