052dd3efbe75fea1de80dddc7005eb312d6ee380
DYefremov <dmitry.v.yefremov@gmail.com>
2021-02-08 22:23:28 +0300

    improved built-in player [added windowed mode]

--- app/tools/media.py
+++ app/tools/media.py
@@ -10,10 +10,10 @@ from app.settings import PlayStreamsMode
 
 
 class Player:
+    """ Simple wrapper for VLC media player. """
     __VLC_INSTANCE = None
-    __PLAY_STREAMS_MODE = PlayStreamsMode.BUILT_IN
 
-    def __init__(self, rewind_callback, position_callback, error_callback, playing_callback):
+    def __init__(self, mode, rewind_cb, position_cb, error_cb, playing_cb):
         try:
             from app.tools import vlc
             from app.tools.vlc import EventType
@@ -21,39 +21,39 @@ class Player:
             log("{}: Load library error: {}".format(__class__.__name__, e))
             raise ImportError
         else:
+            self._mode = mode
             self._is_playing = False
             args = "--quiet {}".format("" if sys.platform == "darwin" else "--no-xlib")
             self._player = vlc.Instance(args).media_player_new()
             ev_mgr = self._player.event_manager()
 
-            if rewind_callback:
+            if rewind_cb:
                 # TODO look other EventType options
                 ev_mgr.event_attach(EventType.MediaPlayerBuffering,
-                                    lambda et, p: rewind_callback(p.get_media().get_duration()),
+                                    lambda et, p: rewind_cb(p.get_media().get_duration()),
                                     self._player)
-            if position_callback:
+            if position_cb:
                 ev_mgr.event_attach(EventType.MediaPlayerTimeChanged,
-                                    lambda et, p: position_callback(p.get_time()),
+                                    lambda et, p: position_cb(p.get_time()),
                                     self._player)
 
-            if error_callback:
+            if error_cb:
                 ev_mgr.event_attach(EventType.MediaPlayerEncounteredError,
-                                    lambda et, p: error_callback(),
+                                    lambda et, p: error_cb(),
                                     self._player)
-            if playing_callback:
+            if playing_cb:
                 ev_mgr.event_attach(EventType.MediaPlayerPlaying,
-                                    lambda et, p: playing_callback(),
+                                    lambda et, p: playing_cb(),
                                     self._player)
 
     @classmethod
-    def get_instance(cls, rewind_callback=None, position_callback=None, error_callback=None, playing_callback=None):
+    def get_instance(cls, mode, rewind_cb=None, position_cb=None, error_cb=None, playing_cb=None):
         if not cls.__VLC_INSTANCE:
-            cls.__VLC_INSTANCE = Player(rewind_callback, position_callback, error_callback, playing_callback)
+            cls.__VLC_INSTANCE = Player(mode, rewind_cb, position_cb, error_cb, playing_cb)
         return cls.__VLC_INSTANCE
 
-    @staticmethod
-    def get_play_mode():
-        return Player.__PLAY_STREAMS_MODE
+    def get_play_mode(self):
+        return self._mode
 
     @run_task
     def play(self, mrl=None):
@@ -80,6 +80,7 @@ class Player:
             self._is_playing = False
             self._player.stop()
             self._player.release()
+            self.__VLC_INSTANCE = None
 
     def set_xwindow(self, xid):
         self._player.set_xwindow(xid)
@@ -115,7 +116,10 @@ class Player:
 
 
 class HttpPlayer:
-    """ Simple wrapper for VLC media player to interact over http. """
+    """ Simple wrapper for VLC media player to interact over http.
+
+        NOT used anymore! May be deleted in the future!
+     """
 
     __VLC_INSTANCE = None
     __PLAY_STREAMS_MODE = PlayStreamsMode.VLC
