1919a387665e02b1c2c40e45f10a363f89053512
DYefremov <dmitry.v.yefremov@gmail.com>
2021-05-15 13:19:20 +0300

    added support for saving single bouquets

--- app/eparser/__init__.py
+++ app/eparser/__init__.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2018-2021 Dmitriy Yefremov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Author: Dmitriy Yefremov
+#
+
 from app.commons import run_task
 from app.settings import SettingsType
 from .ecommons import Service, Satellite, Transponder, Bouquet, Bouquets, is_transponder_valid
@@ -30,6 +57,15 @@ def get_bouquets(path, s_type):
         return BouquetsReader(path).get()
     elif s_type is SettingsType.NEUTRINO_MP:
         return get_neutrino_bouquets(path)
+
+
+def write_bouquet(path, bq, s_type):
+    if s_type is SettingsType.ENIGMA_2:
+        writer = BouquetsWriter(path, None)
+        writer.write_bouquet(path + "userbouquet.{}.{}".format(bq.name, bq.type), bq.name, bq.services)
+    elif s_type is SettingsType.NEUTRINO_MP:
+        from .neutrino.bouquets import write_bouquet
+        write_bouquet(path, bq)
 
 
 @run_task
