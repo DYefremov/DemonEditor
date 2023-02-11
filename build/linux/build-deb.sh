f3cad81a7d26cf2055fbf2ff1a231f419197d82b
DYefremov <dmitry.v.yefremov@gmail.com>
2023-02-11 13:06:13 +0300

    build-deb file update

--- build/linux/build-deb.sh
+++ build/linux/build-deb.sh
@@ -6,6 +6,7 @@ DEB_PATH="$B_PATH/usr/share/demoneditor"
 mkdir -p $B_PATH
 cp -TRv deb $B_PATH
 rsync --exclude=app/ui/lang --exclude=app/ui/icons --exclude=__pycache__ -arv ../../app $DEB_PATH
+rsync --exclude=__pycache__ -arv ../../extensions $DEB_PATH
 
 cd dist
 fakeroot dpkg-deb -Zxz --build DemonEditor
