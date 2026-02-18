0fc0ef1d3e80fc84f4da81e1117db63a1f1d3467
DYefremov <dmitry.v.yefremov@gmail.com>
2026-02-18 15:48:40 +0300

    win build start file update

--- build/win/start.py
+++ build/win/start.py
@@ -4,16 +4,13 @@ import sys
 if __name__ == "__main__":
     if hasattr(sys, "_MEIPASS"):
         import os
-        import pyi_splash
         import ssl
         from multiprocessing import freeze_support
 
         os.environ["PYTHONUTF8"] = "1"
+        freeze_support()
         # TODO There needs to be a more "correct" way.
         ssl._create_default_https_context = ssl._create_unverified_context
-
-        freeze_support()
-        pyi_splash.close()
 
     from app.ui.main import start_app
 
